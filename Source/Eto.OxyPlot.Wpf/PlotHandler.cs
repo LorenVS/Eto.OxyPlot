f341b6c5f721f0029284a784375810e28da034c2
Loren Van Spronsen <lorenvs@outlook.com>
2014-09-21 20:46:08 -0700

    Initial commit, only WPF support so far

---
+++ Source/Eto.OxyPlot.Wpf/PlotHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Drawing;
+using Eto.Wpf;
+using Eto.Wpf.Forms;
+using OxyPlot;
+using OxyPlot.Wpf;
+
+namespace Eto.OxyPlot.Wpf
+{
+    public class PlotHandler : WpfFrameworkElement<PlotView, Plot, Plot.ICallback>, Plot.IHandler
+    {
+        public override Color BackgroundColor
+        {
+            get { return Control.PlotAreaBackground.ToEtoColor(); }
+            set { Control.PlotAreaBackground = value.ToWpfBrush(Control.PlotAreaBackground); }
+        }
+
+        public PlotModel Model
+        {
+            get { return Control.Model; }
+            set { Control.Model = value; }
+        }
+
+        public PlotHandler()
+        {
+            Control = new global::OxyPlot.Wpf.PlotView()
+            {
+            };
+        }
+    }
+}
---
+++ Source/Eto.OxyPlot/Plot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Forms;
+using OxyPlot;
+
+namespace Eto.OxyPlot
+{
+    /// <summary>
+    /// Control to display an OxyPlot plot
+    /// </summary>
+    [Handler(typeof(Plot.IHandler))]
+    public class Plot : Control
+    {
+        new IHandler Handler { get { return (IHandler)base.Handler; } }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Eto.OxyPlot.Plot"/> class.
+        /// </summary>
+        public Plot()
+        {
+        }
+        
+        /// <summary>
+        /// Gets or sets the plot model.
+        /// </summary>
+        /// <value>The plot model.</value>
+        public PlotModel Model
+        {
+            get { return Handler.Model; }
+            set { Handler.Model = value; }
+        }
+
+        /// <summary>
+        /// Handler interface for the <see cref="Plot"/>
+        /// </summary>
+        public new interface IHandler : Control.IHandler
+        {
+            /// <summary>
+            /// Gets or sets the plot model
+            /// </summary>
+            /// <value>The plot model.</value>
+            PlotModel Model { get; set; }
+        }
+    }
+}
