5850a16d0332e7e9a67ffeecac2a6dc4082de434
Menno Deij - van Rijswijk <m.deij@marin.nl>
2016-03-02 08:48:54 +0100

    Updated the publish.cmd to include the new Eto.OxyPlot.Gtk project.

--- Resources/publish.cmd
+++ Resources/publish.cmd
@@ -2,5 +2,6 @@
 
 nuget pack ../Source/Eto.OxyPlot/Eto.OxyPlot.csproj -Prop Configuration=Release
 nuget pack ../Source/Eto.OxyPlot.Wpf/Eto.OxyPlot.Wpf.csproj -Prop Configuration=Release
+nuget pack ../Source/Eto.OxyPlot.Gtk/Eto.OxyPlot.Gtk.csproj -Prop Configuration=Release
 nuget push *.nupkg
 del *.nupkg
